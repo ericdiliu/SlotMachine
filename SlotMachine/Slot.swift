d40e68b8abac03dcf1c702228b231c9c7791b1e1
Eliot Arntz <eliotarntz@Eliots-MacBook-Pro.local>
2014-10-22 12:41:11 +0200

    first commit

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Eliot Arntz on 10/6/14.
+//  Copyright (c) 2014 BitFountain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
